5a6760083d9258dd91a40b5646554d2324f023b5
Dinesh Wickramasinghe <dineshigdd@gmail.com>
2018-12-11 16:58:42 -0800

    new files added for update component

---
+++ src/DBConnection/AddRecord.java
@@ -0,0 +1,140 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DBConnection;
+
+import Model.Contact;
+import Model.Contractor;
+import Model.Freelancer;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author Dinesh
+ */
+public  class AddRecord {
+  //  private DBConnection conn;
+    public final int  FREELANCER = 1;
+    public final int  CONTRACTOR = 0;
+
+    public AddRecord() {
+    }
+    
+    
+    public void setAddress(Contact contact){   //Inserting address        
+        DBConnection  conn = new DBConnection();
+        conn.connectDatabase();
+         try{
+             String query = "";
+             
+                  query = "INSERT INTO Contact( streetAddress,apt,city,state,zip,country,phone,email)" + 
+                                            "VALUES( ? , ? , ? ,? ,? , ? ,?, ? );";
+                                         
+                 PreparedStatement ps = conn.insertRecord(query);
+                                ps.setString( 1, contact.getStreetAddress());
+                                ps.setString( 2, contact.getApt());
+                                ps.setString( 3, contact.getCity());
+                                ps.setString( 4, contact.getState());
+                                ps.setString( 5, contact.getZip());
+                                ps.setString( 6, contact.getCountry());
+                                ps.setString( 7, contact.getPhone());
+                                ps.setString( 8, contact.getEmail());
+                                ps.execute();
+                                
+                                //conn.closeDBConnection();
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+    
+                //conn.connectDatabase();
+                conn.setStatement("select LAST_INSERT_ID();");
+                ResultSet sqlResult  = conn.getStatement();
+                
+                int contactID = 0;
+                try{
+                while( sqlResult.next() ){
+                    contact.setContactId(sqlResult.getInt("LAST_INSERT_ID()"));
+                }
+                conn.closeDBConnection();
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+                  // System.out.println( "Address ID:" + contact.getContactId() );
+               // return addressID;
+               
+    }
+    
+    
+    
+    public void setDbRecord( Object obj, int userType ){
+        
+        DBConnection  conn = new DBConnection();
+        conn.connectDatabase();    
+        
+       
+           // int contactID = setAddress( address );
+            //System.out.println( "Fk key ID:" + contactID );
+        switch( userType ){
+            
+            case CONTRACTOR: 
+                try{
+                Contractor contractor = ( Contractor )obj;
+                
+                String query = "";
+               
+                query = "INSERT INTO Contractor(firstName,lastName,DOB,contractorType,contactID,userID)" + 
+                                            "VALUES( ? , ? , ? ,? ,? , ? );";
+              
+                 System.out.println( "USER ID:" + contractor.getUserAccount().getUserID() );   
+                 
+                 PreparedStatement ps = conn.insertRecord(query);
+                                ps.setString( 1, contractor.getFirstName());
+                                ps.setString( 2, contractor.getLastName());
+                                ps.setTimestamp(3, toTimeStamp(contractor.getDOB()));
+                                ps.setString( 4, contractor.getTypeOfContractor());
+                                ps.setInt( 5, contractor.getContact().getContactId());
+                                ps.setInt( 6, contractor.getUserAccount().getUserID());
+                                ps.execute();
+                 conn.closeDBConnection();                    
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+                break;
+                
+            case FREELANCER:
+                try{
+                Freelancer freelancer = ( Freelancer )obj;
+                
+                String query = "";
+                query = "INSERT INTO Freelancer(firstName,lastName,DOB,yearsOfExperience,selfDescription,contactID,userID)" + 
+                                            "VALUES( ? , ? , ? ,? ,? , ? , ? );";
+                                            
+                                                
+                 PreparedStatement ps = conn.insertRecord(query);
+                                ps.setString( 1, freelancer.getFirstName());
+                                ps.setString( 2, freelancer.getLastName());
+                                ps.setTimestamp(3,toTimeStamp( freelancer.getDOB()));
+                                ps.setString(4, freelancer.getYearsOfExperince());
+                                ps.setString(5, freelancer.getSelfDescription());
+                                ps.setInt( 6, freelancer.getContact().getContactId());
+                                ps.setInt( 7, freelancer.getUserAccount().getUserID());
+                                ps.execute();
+                 conn.closeDBConnection();       
+                }catch( SQLException e){
+                    e.printStackTrace();
+                }
+               
+        }  
+        
+    }
+    
+    private Timestamp toTimeStamp(LocalDate localDate){                
+       return Timestamp.valueOf(localDate.atStartOfDay());
+    }
+}
---
+++ src/Model/UserAccount.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author Dinesh
+ */
+public class UserAccount {
+    private int userID;
+    private String username;
+    private String password;
+    private int userType;
+
+    public UserAccount() {    }
+
+  
+    
+    public UserAccount(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+   public int getUserType() {
+        return userType;
+    }
+
+    public void setUserType(int userType) {
+        this.userType = userType;
+    }
+
+    
+}
